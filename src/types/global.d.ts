e5a887ca3cedb856a646ebaf07389baeb2d13d63
CPK <samkit157@gmail.com>
2023-07-22 03:18:41 +0800

    d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,5 +1,3 @@
-declare const __VERSION__: string;
-
 //https://github.com/pixijs/pixijs/issues/8957
 declare namespace GlobalMixins {
 	interface DisplayObjectEvents extends FederatedEventEmitterTypes {
